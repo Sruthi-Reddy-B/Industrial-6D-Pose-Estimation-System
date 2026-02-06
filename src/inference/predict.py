274ce00ad825d94434ca99dee6c9b79ae0d2a913
B Sruthi Reddy <sruthireddy.bapa@gmail.com>
2026-02-06 14:48:28 +0100

    Update predict.py

--- src/inference/predict.py
+++ src/inference/predict.py
@@ -1,1 +1,11 @@
+import torch
+from src.models.model import SimplePoseModel
+
+def predict(model_path, image_tensor, device='cpu'):
+    model = SimplePoseModel().to(device)
+    model.load_state_dict(torch.load(model_path, map_location=device))
+    model.eval()
+    with torch.no_grad():
+        pred = model(image_tensor.unsqueeze(0).to(device))
+    return pred.squeeze(0)
 
