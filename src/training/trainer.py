8e9e2b761a6e697a7127415be1c47b3063d986bb
B Sruthi Reddy <sruthireddy.bapa@gmail.com>
2026-02-06 14:47:40 +0100

    Update trainer.py

--- src/training/trainer.py
+++ src/training/trainer.py
@@ -1,1 +1,13 @@
+def train_one_epoch(model, loader, optimizer, loss_fn, device='cpu'):
+    model.train()
+    total_loss = 0
+    for x, y in loader:
+        x, y = x.to(device), y.to(device)
+        optimizer.zero_grad()
+        pred = model(x)
+        loss = loss_fn(pred, y)
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item()
+    return total_loss / len(loader)
 
